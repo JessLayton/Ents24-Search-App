f3925c7ddaffc0a97ece43f4ce75a2892dea2d68
Jess <jessylayton@yahoo.co.uk>
2023-01-22 17:22:40 +0000

    story for bold results item with many events

--- src/components/ResultsItem.stories.jsx
+++ src/components/ResultsItem.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ResultsItem from './ResultsItem';
 import resultsItem from '../../exampleData/resultsItem.json';
+import boldResultsItem from '../../exampleData/boldResultsItem.json';
 
 export default {
   title: 'Results Item',
@@ -17,10 +18,15 @@ export default {
 };
 
 const Template = (args) => {
-  return <ResultsItem {...args} />;
+  return <div style={{padding: '20px 350px'}}><ResultsItem {...args} /></div>;
 };
 
 export const DefaultResultsItem = Template.bind({});
 DefaultResultsItem.args = {
   item: resultsItem,
+};
+
+export const BoldResultsItem = Template.bind({});
+BoldResultsItem.args = {
+  item: boldResultsItem,
 };
